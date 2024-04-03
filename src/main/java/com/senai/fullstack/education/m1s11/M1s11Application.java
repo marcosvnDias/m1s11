bf66fb2a7fc5c8ff05634fb0da3d8b752ac5d6c8
Marcos Vinicios <mvdias1268@gmail.com>
2024-04-03 10:44:02 -0300

    first commit

---
+++ src/main/java/com/senai/fullstack/education/m1s11/M1s11Application.java
@@ -0,0 +1,13 @@
+package com.senai.fullstack.education.m1s11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M1s11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M1s11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/senai/fullstack/education/m1s11/M1s11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.fullstack.education.m1s11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M1s11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
